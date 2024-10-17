6fd233ea95e8168efd626d417d66d3e855d8349d
Saman <119083568+navidasaman@users.noreply.github.com>
2024-10-17 22:08:58 +0200

    created about page and cleaned up code and css

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>about.</h1>
+    <p>
+      This vue app was created with the purpose of experimenting with the Composition API 
+      in relation to previous application made with Options API
+    </p>
   </div>
 </template>
+
+<style scoped>
+  .about {
+    padding:50px;
+    color:#3D52A0;
+    width:30vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin:auto;
+    background-color: whitesmoke;
+    padding:20px;
+    border-radius: 10px;
+    margin-top: 50px;
+    box-shadow: 1px 2px 2px grey;
+  }
+
+  @media screen and (max-width: 800px) {
+    .about {
+      width:100vw;
+      max-width: 80%;
+    }  
+  }
+</style>
